41ef0f6dc5ab9131f597c8d48ac69034492ea0f3
Kristin <kristindyulgeryan@abv.bg>
2025-06-10 15:28:27 +0300

    add generic utility type `Choose` for selective property extraction.

---
+++ 03-Generics/ChooseType.ts
@@ -0,0 +1,16 @@
+type Choose<T, K extends keyof T> = {
+  [Key in K]: T[Key];
+};
+
+type anotherType = {
+  time: Date;
+  duration: number;
+  test: () => string;
+  val: 200 | 300;
+  user: {
+    name: string;
+    age: number;
+  };
+};
+type nestedUserAndTime = "user" | "time";
+type extracted2 = Choose<anotherType, nestedUserAndTime>;
